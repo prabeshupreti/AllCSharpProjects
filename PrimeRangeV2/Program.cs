535c5be11e4d43ea9d8d0ab7a5e936acfa1e3395
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 18:45:45 +0545

    Project to find the prime numbers from given numbers, version 2.

---
+++ PrimeRangeV2/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+int BeginRange, EndRange;
+
+Console.Write("Enter the start range: ");
+bool IsNumber = int.TryParse(Console.ReadLine(), out BeginRange);
+
+if (!IsNumber)
+{
+    Console.WriteLine("The number you entered is not an integer.");
+    return;
+}
+
+Console.Write("Enter the end range: ");
+IsNumber = int.TryParse(Console.ReadLine(), out EndRange);
+
+if (!IsNumber)
+{
+    Console.WriteLine("The number you entered is not an integer.");
+    return;
+}
+
+for (int i = BeginRange; i <= EndRange; i++) 
+{
+    bool IsPrime = true;
+    if (i <= 1)
+    {
+        IsPrime = false;
+    }
+    else if (i == 2)
+    {
+        IsPrime = true;
+    }
+    else
+    {
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                IsPrime = false;
+                break;
+            }
+        }
+    }
+    if (IsPrime) 
+    {
+        Console.WriteLine(i);
+    }
+}
