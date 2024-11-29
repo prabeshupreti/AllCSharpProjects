4f29d44c6ef2eedc7e7af97711f426040d1e0c41
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 19:35:43 +0545

    Evaluating the version 1 of the prime numbers range algorithm, version 1.

---
+++ PrimeNumberAlgorithmEvaluationV1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Diagnostics;
+
+int MaxRange = 900000;
+Stopwatch Stopwatch = Stopwatch.StartNew();
+
+Stopwatch.Start();
+for (int i = 1; i <= MaxRange; i++)
+{
+    bool IsPrime = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            IsPrime = false;
+            break;
+        }
+    }
+}
+
+Stopwatch.Stop();
+
+//Will normally take more than 2 minutes.
+Console.WriteLine($"Elapsed time to evaluate {MaxRange} is {Stopwatch.Elapsed.ToString(@"mm\:ss\:fffffff")}.");
