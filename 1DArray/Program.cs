2e6b670c310693dc81836d3c1b796855f12f131a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 13:55:24 +0545

    1D array demonstration.

---
+++ 1DArray/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int[] Numbers = new int[5];
+
+#region Using for loop
+
+for (int i = 0; i < Numbers.Length; i++) 
+{
+    Numbers[i] = i;
+}
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    Console.WriteLine(Numbers[i]);
+}
+Console.WriteLine();
+
+#endregion
+
+#region Using for each loop
+
+foreach (int i in Enumerable.Range(0, Numbers.Length))
+{
+    Numbers[i] = i;
+}
+
+foreach (int Number in Numbers)
+{
+    Console.WriteLine(Number);
+}
+Console.WriteLine();
+
+#endregion
