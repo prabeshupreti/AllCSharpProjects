a625d439520d81a93b524c2714556174b6a8ab43
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 14:29:34 +0545

    Finding the greatest number using array, version 1.

---
+++ GreatestNumberV1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] Numbers = new int[5];
+
+Console.WriteLine("Enter the numbers: ");
+
+foreach (int i in Enumerable.Range(0, Numbers.Length).ToArray()) 
+{
+    Numbers[i] = int.Parse(Console.ReadLine());
+}
+
+int Max = Numbers[0];
+
+foreach (int i in Enumerable.Range(1, Numbers.Length - 1).ToArray())
+{
+    if (Max < Numbers[i]) 
+    {
+        Max = Numbers[i];
+    }
+}
+
+Console.WriteLine($"The greatest number is {Max}.");
+
