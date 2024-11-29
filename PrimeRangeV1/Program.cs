d8f437a6b5f8e90d8a45b5956bc814c5235de1ce
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 18:39:55 +0545

    Project to print range of prime numbers.

---
+++ PrimeRangeV1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int BeginRange, EndRange;
+
+Console.Write("Enter the start range: ");
+BeginRange = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the end range: ");
+EndRange = int.Parse(Console.ReadLine());
+
+for (int i = BeginRange; i <= EndRange; i++) 
+{
+    bool IsPrime = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            IsPrime = false;
+            break;
+        }
+    }
+    if (IsPrime) 
+    {
+        Console.WriteLine(i);
+    }
+}
