276e79cf600f68e2f96e1e280b6682e5c35ebf4a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 13:47:38 +0545

    Example of tuple. Expand the region to see codes.

---
+++ Tuple/Program.cs
@@ -0,0 +1,34 @@
+﻿#region Tuple Definition
+
+using Tuple = (int Programming, int Networking);
+
+#endregion
+
+#region Input
+Tuple t1;
+
+Console.WriteLine("Enter the marks: ");
+t1 = (int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine());
+
+#endregion
+
+#region Boxing
+
+Tuple t2;
+t2 = (t1.Programming, t2.Networking);
+
+#endregion
+
+#region Unboxing
+
+Tuple t3;
+(t3.Programming, t3.Networking) = t2;
+
+#endregion
+
+#region Output
+
+Console.WriteLine($"Programming:{t3.Programming}, Networking:{t3.Programming}");
+
+#endregion
+
