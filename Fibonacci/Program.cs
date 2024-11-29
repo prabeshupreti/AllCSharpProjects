44bc817961d90e4021ee9fda130d55ef5b357efb
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 13:35:58 +0545

    Fibonacci Series.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿int range;
+int First = 1, Second = 1;
+int Next;
+
+Console.Write("Enter the range you want to print of: ");
+range = int.Parse(Console.ReadLine());
+
+Console.Write($"{First}, {Second}");
+
+for (int i = 3; i <= range; i++) 
+{
+    Next = First + Second;
+    Console.Write($", {Next}");
+    First = Second;
+    Second = Next;
+}
