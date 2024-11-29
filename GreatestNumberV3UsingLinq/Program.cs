d6eadbb101c6bdc6f7fa2f118ce39ef850388fca
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 14:55:55 +0545

    New name.

---
+++ GreatestNumberV3UsingLinq/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter 5 numbers: ");
+
+int[] Numbers = Enumerable.Range(0, 5)
+                           .Select(_ => int.Parse(Console.ReadLine()))
+                           .ToArray();
+
+Console.WriteLine($"The greatest number is {Numbers.Max()}.");
