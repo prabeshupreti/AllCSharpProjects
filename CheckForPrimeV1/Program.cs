e58ed0c9e0e57854ec913c6ff0ecb121ecb4f6ce
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-11-29 14:38:51 +0545

    Checking if a number is prime, version 1.

---
+++ CheckForPrimeV1/Program.cs
@@ -0,0 +1,23 @@
+﻿int Number = 0;
+bool IsPrime = true;
+
+Console.Write("Enter the number: ");
+Number = int.Parse(Console.ReadLine());
+
+for (int i = 2; i < Number; i++)
+{
+    if (Number % i == 0)
+    {
+        IsPrime = false;
+        break;
+    }
+}
+
+if (IsPrime)
+{
+    Console.WriteLine($"{Number} is a prime number.");
+}
+else
+{
+    Console.WriteLine($"{Number} is not a prime number.");
+}
