717206bbbb4fca19b7a4eda707b45d16c8a09f0f
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-06 12:46:04 +0545

    Factorial function project to print factorials.

---
+++ FibonacciWithFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿double range;
+
+Console.Write("Enter the range you want to print of: ");
+range = double.Parse(Console.ReadLine());
+
+Fibonacci(range);
+
+void Fibonacci(double range)
+{
+    double First = 1, Second = 1;
+    double Next;
+    Console.Write($"{First}, {Second}");
+
+    for (int i = 3; i <= range; i++)
+    {
+        Next = First + Second;
+        Console.Write($", {Next}");
+        First = Second;
+        Second = Next;
+    }
+}
