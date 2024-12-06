352cbcfea0927102c6490e71e9094b614f6a7e0b
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-06 12:44:32 +0545

    Added factorial project with function.

---
+++ FactorialWithFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿int Number;
+
+Console.Write("Enter the number: ");
+Number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The factorial of {Number} is {Factorial(Number)}.");
+
+int Factorial(int N)
+{
+    int F = 1;
+    for (int i = N; i >= 1; i--)
+    {
+        F *= i;
+    }
+    return F;
+}
