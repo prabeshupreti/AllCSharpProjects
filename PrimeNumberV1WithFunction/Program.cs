6b08c56d7a43fa0479b4089db0061d8a476c0734
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-13 10:33:32 +0545

    Prime number version 1 with function.

---
+++ PrimeNumberV1WithFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int Number;
+
+Console.Write("Enter the number: ");
+Number = int.Parse(Console.ReadLine());
+
+bool IsPrime = CheckForPrime(Number);
+
+if (IsPrime)
+{
+    Console.WriteLine($"{Number} is a prime number.");
+}
+else
+{
+    Console.WriteLine($"{Number} is not a prime number.");
+}
+
+bool CheckForPrime(int N) 
+{
+    bool IsPrime = true;
+
+    for (int i = 2; i < N; i++)
+    {
+        if (N % i == 0)
+        {
+            IsPrime = false;
+            break;
+        }
+    }
+    return IsPrime;
+}
