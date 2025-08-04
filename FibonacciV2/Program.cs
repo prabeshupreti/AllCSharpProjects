7471ccd2c2bffe23fe0c77696c9b83412f3323ce
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-08-04 15:29:07 +0545

    Added Version 2 of printing Fibonacci sequence.

---
+++ FibonacciV2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+var fibonacciSeries = Enumerable.Range(0, 10)
+            .Select(n => (int)Math.Round((Math.Pow((1 + Math.Sqrt(5)) / 2, n) - Math.Pow((1 - Math.Sqrt(5)) / 2, n)) / Math.Sqrt(5)));
+
+foreach (int i in fibonacciSeries) 
+{
+    Console.WriteLine($"{i} ");
+}
