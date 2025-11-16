29ff9d6fa7bc1d9a18491b79cf86b7d5b49af889
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:28:37 +0545

    Calculate fibonacci using recursion.

---
+++ RecursionFibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
+
+int Fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
