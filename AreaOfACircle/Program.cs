dd0298faa8c9d8b509d2e49701c4aca429771c93
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:26:51 +0545

    Calculate area of a circle.

---
+++ AreaOfACircle/Program.cs
@@ -0,0 +1,13 @@
+﻿
+double Radius;
+double Area;
+
+const double PI = (float)355 / 113;
+
+Console.Write("Enter the radius of a circle: ");
+Radius = double.Parse(Console.ReadLine());
+
+Area = PI * Radius * Radius;
+
+Console.WriteLine($"The area of a circle is {Math.Round(Area, 2)}.");
+
