4ad3b97631d52f0fef59cb1f34a518ac162b25cf
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:29:46 +0545

    calculate the sum from the intergers in the array.

---
+++ SumFromArray/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] Numbers = new int[5];
+int Smallest = 0;
+
+Console.WriteLine("Enter the numbers: ");
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    Numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Smallest = Numbers[0];
+
+for (int i = 1; i < Numbers.Length; i++)
+{
+    if (Numbers[i] <= Smallest)
+    {
+        Smallest = Numbers[i];
+    }
+}
+
+Console.WriteLine($"The Smallest number is {Smallest}.");
