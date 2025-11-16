ab3ccfbe616025328a67eab370a51244f313c514
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:27:31 +0545

    add from bitwise operator.

---
+++ BitwiseAddition/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int a = 10;
+int b = 5;
+
+while (b != 0)
+{
+    int carry = (a & b) << 1; // find carry bits
+    a = a ^ b;                // add without carry
+    b = carry;                // move carry to b
+}
+
+Console.WriteLine(a);
