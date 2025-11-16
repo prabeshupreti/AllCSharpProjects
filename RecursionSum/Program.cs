aec2c7f833ce737a4b0de0f1efc7b8778de5fedf
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:29:28 +0545

    Add natural numbers using recursion.

---
+++ RecursionSum/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int result = SumNaturalNumbers(5);
+
+int SumNaturalNumbers(int N)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return N + SumNaturalNumbers(N - 1);
+    }
+}
