4cb4d2c83d796dd8ffa611b38ac6ed14bc40bb92
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:27:41 +0545

    Subtract using bitwise.

---
+++ BitwiseSubtraction/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int a = 20;
+int b = 7;
+
+// Convert subtraction into: a + (~b + 1)
+b = ~b + 1; // two's complement (negation)
+
+// Perform addition using bitwise logic
+while (b != 0)
+{
+    int carry = (a & b) << 1;
+    a = a ^ b;
+    b = carry;
+}
+
+Console.WriteLine(a); // 13
