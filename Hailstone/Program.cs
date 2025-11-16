ed449bb005e7638df767c398aa8cf829e2237e7b
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 11:44:19 +0545

    Hailstone code example.

---
+++ Hailstone/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Number = 9;
+
+Console.Write(Number);
+
+while (Number != 1)
+{
+    if (Number % 2 != 0)
+    {
+        Number = 3 * Number + 1;
+    }
+    else
+    {
+        Number = Number / 2;
+    }
+    Console.Write($", {Number}");
+}
+
