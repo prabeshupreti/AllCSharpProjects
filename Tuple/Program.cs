418d83ef260a9625ae159e4638fff4f47a178115
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 13:33:27 +0545

    Written basic tuple example over old one to help understand easily.

--- Tuple/Program.cs
+++ Tuple/Program.cs
@@ -1,34 +1,9 @@
-﻿#region Tuple Definition
+﻿
+int A = 4;
+int B = 7;
 
-using Tuple = (int Programming, int Networking);
+Console.WriteLine($"Before Swap: A = {A}, B = {B}");
 
-#endregion
+(A, B) = (B, A); //Tuple representation for swapping
 
-#region Input
-Tuple t1;
-
-Console.WriteLine("Enter the marks: ");
-t1 = (int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine());
-
-#endregion
-
-#region Boxing
-
-Tuple t2;
-t2 = (t1.Programming, t2.Networking);
-
-#endregion
-
-#region Unboxing
-
-Tuple t3;
-(t3.Programming, t3.Networking) = t2;
-
-#endregion
-
-#region Output
-
-Console.WriteLine($"Programming:{t3.Programming}, Networking:{t3.Programming}");
-
-#endregion
-
+Console.WriteLine($"After Swap: A = {A}, B = {B}");
