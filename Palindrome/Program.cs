9d0197a64b8f712eaf685e0ca924bc7c18dded02
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 11:43:16 +0545

    Code for Palindrome.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int Number;
+
+Console.Write("Enter a number: ");
+Number = int.Parse(Console.ReadLine());
+
+int OriginalNumber = Number;
+int ReversedNumber = 0;
+
+while (Number != 0)
+{
+    int Remainder = Number % 10;
+    ReversedNumber = ReversedNumber * 10 + Remainder;
+    Number = Number / 10;
+}
+
+if (OriginalNumber == ReversedNumber)
+{
+    Console.WriteLine("The given number is palindrome.");
+}
+else
+{
+    Console.WriteLine("The given number is not palindrome.");
+}
