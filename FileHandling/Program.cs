87233143083411308a423ce75522a77266d1e4c0
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 11:38:44 +0545

    Basic file handling example.

---
+++ FileHandling/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Tell us about yourself: ");
+
+string userInput = Console.ReadLine() ?? "";
+
+
+File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Personal)+@"\myDoc.txt", userInput);
